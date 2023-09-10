d4dd7488552b19207fd0ebff3aaf2424b19965ef
haku <gentlebuuny@gmail.com>
2023-09-10 10:07:28 +0900

    refacotr: use 'model_config'

--- dofi/settings/env.py
+++ dofi/settings/env.py
@@ -1,20 +1,21 @@
 from functools import lru_cache
 from pathlib import Path
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
 
 
 class EnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=f"{ROOT_DIR}/.env",
+        env_file_encoding="utf-8",
+    )
+
     # API tokens
     GITHUB_PAT: str | None = None
     # Database
     SQLITE_DB_PATH: Path
-
-    class Config:
-        env_file = f"{ROOT_DIR}/.env"
-        env_file_encoding = "utf-8"
 
 
 @lru_cache(maxsize=1)
